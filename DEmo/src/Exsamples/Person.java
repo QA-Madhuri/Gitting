c47cd769e08871483427b7b123e910edda26436d
SK <SK@192.168.1.117>
2022-01-31 10:45:21 +0530

    im commiting the base code

---
+++ DEmo/src/Exsamples/Person.java
@@ -0,0 +1,22 @@
+package Exsamples;
+
+public class Person {
+
+	//datatype vrblname=value;
+	//global/class-levl variables
+
+	
+	public static void main(String[] args) {
+		//local varibales
+		int a=1;
+		System.out.println(a++);
+		System.out.println(a);//a=2
+		System.out.println(++a);//2+1=a
+		System.out.println(a--);//a=2, returned 3
+		System.out.println(--a);//decrement and then returned
+		
+		
+		
+	}
+
+}
